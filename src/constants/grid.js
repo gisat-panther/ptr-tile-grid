e795961033b08ca29627d66e446d9f89ef7bc507
Vojtěch Dubrovský <v.dubr@hotmail.com>
2020-05-20 09:15:18 +0200

    Generate resolutions for 25 levels

--- src/constants/grid.js
+++ src/constants/grid.js
@@ -18,8 +18,27 @@ const getGridExtent = () => {
 
 const EARTH_CIRCUIT = 40075000 //in meters
 
+//Tile size in degrees
+const LEVEL_0_TILE_SIZE = BASE_SIZE / 2;
+
+//size of tile in pixels
+const PIXEL_TILE_SIZE = 250;
+
+//resolutions
+//Resolution is number of meters displayed inone pixel.
+
+// Maximum resolution for level 0. Level 0 has two tiles of size 180° x 180°
+const maxResolution = EARTH_CIRCUIT / PIXEL_TILE_SIZE;
+const resolutions = []
+const levels = 25;
+for (let i = 0; i < levels; ++i) {
+	resolutions[i] = maxResolution / Math.pow(2, i + 1);
+}
+
 export default {
 	getGridExtent,
 	BASE_SIZE,
 	LEVEL_BOUNDARIES,
+	LEVEL_0_TILE_SIZE,
+	resolutions,
 }
---
+++ tests/constants/constants-test.js
@@ -0,0 +1,18 @@
+import {assert} from 'chai';
+import {
+    constants
+} from '../../src/';
+
+describe('constants/constants', function () {
+	describe('resolutions', function () {
+        it('Check first and last resolution', function () {
+			assert.equal(constants.resolutions[0], 80150);
+			assert.equal(constants.resolutions[1], 40075);
+			assert.equal(constants.resolutions[2], 20037.5);
+			assert.equal(constants.resolutions[20], 80150 / Math.pow(2, 20));
+			assert.equal(constants.resolutions[24], 80150 / Math.pow(2, 24));
+			assert.equal(constants.resolutions[25], 80150 / Math.pow(2, 25));
+			assert.equal(constants.resolutions.length, 25);
+		});
+    });
+});
